c4735535cdbc95d0122f2e9e61fffc12c56a691d
Amelia Putri Aftiana <117893334+ameliaaftiana@users.noreply.github.com>
2026-06-17 15:48:31 +0700

    Initial Commit

---
+++ PanelSyncc/PanelSynccApp.swift
@@ -0,0 +1,17 @@
+//
+//  PanelSynccApp.swift
+//  PanelSyncc
+//
+//  Created by Amelia Putri Aftiana on 17/06/26.
+//
+
+import SwiftUI
+
+@main
+struct PanelSynccApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
